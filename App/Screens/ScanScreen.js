02d440d7ebb91d1fd2e27fdcf472c55a47415990
yarethmelendez <yareth.melendez@yahoo.com>
2021-11-27 01:35:09 -0400

    Scan screen with navigation

--- App/Screens/ScanScreen.js
+++ App/Screens/ScanScreen.js
@@ -1,53 +1,86 @@
+///////////////////no ////////////
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Button,
+  TouchableOpacity,
+  Switch
+} from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import firestore from '../../firebase';
 import firebase from 'firebase';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { FontAwesome, MaterialCommunityIcons } from '@expo/vector-icons';
 
 /////SE SUPONE QUE FUNCIONE
-export default function ScanScreen() {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [scanned, setScanned] = useState(false);
-
-  useEffect(() => {
-    (async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
-  }, []);
-
-  const handleBarCodeScanned = ({data}) => {
-    setScanned(true);
-    //escribir base de datos
-    let user = firebase.auth().currentUser;
-    let ref = firestore.collection('users').doc(user.uid).collection('contacts').doc(data);
-    let userId = ref.set({
-       userContact :data
-    });
-    alert(`Your contact was saved!`);
-
-  };
-
-  if (hasPermission === null) {
-    return <Text>Requesting camera permission</Text>;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
+export default function ScanScreen({navigation, route}, props) {
+  const scanQR= ()=> { navigation.navigate('QR Code Scanner')};
+  const scanCard= ()=> { navigation.navigate('Business Card Scanner')};
 
   return (
-    <View
-      style={{
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'flex-end',
-      }}>
-      <BarCodeScanner
-        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-        style={StyleSheet.absoluteFillObject}
-      />
+    <View style= {styles.container}> 
+      <View style = {styles.cardScan}>
+        <Text>Business Card Scanner</Text>
+      <TouchableOpacity onPress={scanCard}>
+          <Icon
+            name="card"
+            color='black'
+            size={50}
+          />
+        </TouchableOpacity>
+      </View>
 
-      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+      <View style = {styles.qrCode}>
+      <Text>QR Code Scanner</Text>
+      <TouchableOpacity onPress={scanQR}>
+          <FontAwesome
+           
+            name="qrcode"
+            color='black'
+            size={50}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+
+container: {
+  flex:1,
+  margin: 10
+
+
+},
+cardScan: {
+  width: '50%',
+  height: '50%',
+  backgroundColor:'#d3d3d3',
+  borderRadius: 30,
+  justifyContent: 'center',
+  alignItems:'center',
+  borderWidth: 1,
+  borderColor: '#a6a2a2'
+},
+qrCode: {
+  alignSelf:'flex-end',
+  width: '50%',
+  height: '50%',
+  backgroundColor:'#d3d3d3',
+  borderRadius: 30,
+  justifyContent: 'center',
+  alignItems:'center',
+  borderWidth: 1,
+  borderColor: '#a6a2a2',
+  
+  
+},
+
+
+});
+
